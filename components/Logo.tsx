5ae9048d088e1a2a66a2b70f5aef06913cd06d38
FatimaSanchez413 <sanchezfatimarose13@gmail.com>
2024-12-30 00:14:06 +0800

    authentication and themes

---
+++ components/Logo.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import { HandCoins } from 'lucide-react';
+import React from 'react'
+
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+    <HandCoins className="stroke h-11 w-11 stroke-green-500
+    stroke-[1.5]" />
+
+    <p className="bg-gradient-to-r from-green-400 to-yellow-500
+    bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        PennyWise
+        </p>
+    </a>
+  )
+}
+
+export default Logo;
