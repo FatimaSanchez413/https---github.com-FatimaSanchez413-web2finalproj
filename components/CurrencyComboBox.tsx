d87ac431d11bd0485f58291a832f355e071b05c2
FatimaSanchez413 <sanchezfatimarose13@gmail.com>
2024-12-30 17:54:44 +0800

    Finished Wizard Page (Updated BackEnd)

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 import * as React from "react"
@@ -22,18 +23,22 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
-import { Currencies, Currency } from "@/lib/currencies"
-import { useQuery } from "@tanstack/react-query"
+import { Currencies, Currency as AppCurrency } from "@/lib/currencies"
+import { useQuery, useMutation, Mutation } from "@tanstack/react-query"
 import SkeletonWrapper from "./SkeletonWrapper"
 import { UserSettings } from "@prisma/client"
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings"
+import { Currency } from "lucide-react"
+import { toast } from "sonner"
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false)
   const isDesktop = useMediaQuery("(min-width: 768px)")
   const [selectedOption, setSelectedOption] = React.
-  useState<Currency | null>(
+  useState<AppCurrency | null>(
     null
   );
+  
 
   const userSettings =  useQuery<UserSettings>({
     queryKey: ["userSettings"],
@@ -48,50 +53,90 @@ export function CurrencyComboBox() {
     if(userCurrency) setSelectedOption(userCurrency);
  }, [userSettings.data]);
 
+const mutation = useMutation({
+  mutationFn: UpdateUserCurrency,
+  onSuccess: (data: UserSettings) => {
+    toast.success('Currency updated successfully 🎉', {
+      id: "update-currency",
+    });
 
+    setSelectedOption(
+      Currencies.find(c => c.value === data.currency) || null
+    );
+  },
+  onError: (e) => {
+    console.error(e);
+    toast.error("Something went wrong", {
+      id: "update-currency",
+    });
+  },
+});
+
+const selectOption = React.useCallback((currency: AppCurrency | null) => {
+  if(!currency) {
+    toast.error("Please select a currency");
+    return;
+  }
+  toast.loading("Updating currency...", {
+    id: "update-currency",
+  });
+
+  mutation.mutate(currency.value);
+},
+[mutation]
+);
 
   if (isDesktop) {
     return (
       <SkeletonWrapper isLoading={userSettings.isFetching}>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
+          <Button 
+          variant="outline" 
+          className="w-full justify-start"
+          disabled={mutation.status === 'pending'}
+          >
             {selectedOption ? <>{selectedOption.label}</> :
              <> + Set your currency</>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
+          <OptionList setOpen={setOpen} setSelectedOption={selectOption} isDesktop={isDesktop} />
         </PopoverContent>
       </Popover>
       </SkeletonWrapper>
     )
   }
-
+  
   return (
     <SkeletonWrapper isLoading={userSettings.isFetching}>
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-[150px] justify-start">
-          {selectedOption ? <>{selectedOption.label}</> : <>+ Set status</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </div>
-      </DrawerContent>
-    </Drawer>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button
+            variant="outline"
+            className="w-full justify-start"
+            disabled={mutation.status === 'pending'}
+          >
+            {selectedOption ? <>{selectedOption.label}</> : <> + Set your currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent className="w-[200px] p-0">
+          <OptionList setOpen={setOpen} setSelectedOption={selectOption} isDesktop={isDesktop} />
+        </DrawerContent>
+      </Drawer>
     </SkeletonWrapper>
   )
-}
+  }
 
+  
 function OptionList({
   setOpen,
   setSelectedOption: setSelectedOption,
+  isDesktop,
 }: {
   setOpen: (open: boolean) => void
-  setSelectedOption: (status: Currency | null) => void
+  setSelectedOption: (status: AppCurrency | null) => void
+  isDesktop: boolean
 }) {
   return (
     <Command>
@@ -99,7 +144,7 @@ function OptionList({
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup>
-          {Currencies.map((currency:Currency) => (
+          {Currencies.map((currency: AppCurrency) => (
             <CommandItem
               key={currency.value}
               value={currency.value}
